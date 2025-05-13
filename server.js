a2fe1e056936915489ad6e9afef8a25ce38b098d
Nada <nada@Nadas-MacBook-Air.local>
2025-05-13 15:41:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
