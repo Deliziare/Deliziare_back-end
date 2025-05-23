6cd6173ccb356bbab7569207c5b74a68cb9a90a4
ijjaasss <ijasmuthu04@gmail.com>
2025-05-23 16:48:39 +0530

    feat(user):accept bid

--- Controller/bidController.js
+++ Controller/bidController.js
@@ -1,10 +1,10 @@
-import { createBid, getChefBids } from "../Service/bidService.js";
+import Bid from "../Models/bidModel.js";
+import { createBid, getBidsForPost, getChefBids } from "../Service/bidService.js";
 
 export const createBidController = async (req, res) => {
   try {
     const { postId, bidAmount } = req.body;
     const chefId = req.user.id;
-
     const bid = await createBid({ postId, chefId, bidAmount });
     res.status(201).json(bid);
   } catch (error) {
@@ -23,3 +23,48 @@ export const getChefBidsController = async (req, res) => {
   }
 };
 
+export const getUserBidReplays=async (req,res)=>{
+    try {
+   
+      const {postId}=req.query;
+     
+      if(!postId){
+        res.status(400).json({ message: 'post id is required ' });
+      }
+    const bids= await getBidsForPost(postId)
+      res.json(bids);
+    } catch (error) {
+       res.status(500).json({ message: 'Failed to fetch ' ,error});
+    }
+}
+
+export const AcceptBid=async (req,res)=>{
+ const { bidId ,postId } = req.body;
+
+  try {
+    if(!bidId||!postId){
+      return res.status(400).json({message:"bid id and post id is requird"})
+    }
+    const acceptedBid = await Bid.findByIdAndUpdate(
+      bidId,
+      { status: 'accepted' },
+      { new: true }
+    );
+
+    await Bid.updateMany(
+      {
+        postId,
+        _id: { $ne: bidId }
+      },
+      { $set: { status: 'rejected' } }
+    );
+
+    res.status(200).json({
+      message: 'Bid accepted and other bids rejected',
+      bid: acceptedBid
+    });
+  } catch (error) {
+    console.error('Error processing bid:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
