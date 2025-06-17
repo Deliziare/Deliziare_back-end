563c15123f4ffb8739cc0f0edaeabbd4dd460dcb
ijjaasss <ijasmuthu04@gmail.com>
2025-06-17 19:38:15 +0530

    fetch all data

--- Routes/messageRoutes.js
+++ Routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { findUserForChat, getChatUsers, getMessage, sendMessage } from '../Controller/messageController.js';
+import { addAddressToChatRequest, findUserForChat, getChatUsers, getMessage, markMessagesAsRead, rejectRequstMessage, sendMessage } from '../Controller/messageController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 
@@ -11,4 +11,8 @@ router.get('/get-message/:userId/:otherUserId',verifyToken, getMessage);
 router.post('/sendMessage',verifyToken,sendMessage);
 router.get('/find-user/:id',verifyToken,findUserForChat)
 router.get('/get-chat-users',verifyToken,getChatUsers)
+router.post('/markAsRead/:userId/:senderId',verifyToken,markMessagesAsRead)
+router.post('/rejectreqmessage',verifyToken,rejectRequstMessage)
+router.put('/add-address/:requestId', verifyToken, addAddressToChatRequest);
+
 export default router;
